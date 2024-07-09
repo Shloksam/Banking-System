2a3821b2b98544a6ea4ea8eeda20ad499ef95dc4
Shloksam <111415904+Shloksam@users.noreply.github.com>
2024-07-09 18:46:06 +0530

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,29 @@
+package BankingManagementSystem;
+
+import java.util.Scanner;
+
+class bank{
+
+    public static void main(String [] args){
+
+        while(true){
+
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+            switch(n){
+                case 1:
+                System.out.println(n);
+                case 2:
+                System.out.println(n);
+                case 3:
+                System.out.println(n);
+                case 4:
+                System.out.println(n);
+                case 5:
+                System.out.println(n);
+            }
+            
+        }
+    }
+}
