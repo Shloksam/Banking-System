4d5a15b57bf1e29a89c806e10e8074dd9cce2ec6
Shloksam <111415904+Shloksam@users.noreply.github.com>
2024-10-24 18:57:35 +0530

    Update Accounts.java

--- Accounts.java
+++ Accounts.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Accounts {
     private Connection connection;
     private Scanner scanner;
+    
     public Accounts(Connection connection, Scanner scanner){
         this.connection = connection;
         this.scanner = scanner;
